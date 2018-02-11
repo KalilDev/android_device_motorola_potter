a0b42a886c813411308fd9720825129d06784013
Boy Petersen <boypetersen@gmail.com>
2018-02-11 17:42:52 +0100

    increase volume steps for voice calls and media

--- device.mk
+++ device.mk
@@ -306,4 +306,8 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.vc_call_vol_steps=7 \
+	ro.config.media_vol_steps=20
+
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
